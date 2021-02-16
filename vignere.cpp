7b886b54b26a4e53da7e2c2daca824c447e80411
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2021-02-16 16:51:32 +0530

    VIGNERE modified

--- vignere.cpp
+++ vignere.cpp
@@ -1,64 +1,42 @@
-//Vigenere Cipher Implementation in C++
+// vignere algorithms
+// MOHAMMAD FAISAL
+// https://ideone.com/W351P5
+
+
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
-
-//Cyclically maps key to input string size
-string generateKey(string str, string key)
-{
-	int x = str.size();
-	for (int i = 0; ; i++)
-	{
-		if (x == i)
-			i = 0;
-		if (key.size() == str.size()) //size check
-			break;                  
-		key.push_back(key[i]);        
-	}
-	return key;
-}
-
-//Encyption function
-string cText(string str, string key)
-{
-	string cipher_text;
-
-	for (int i = 0; i < str.size(); i++)
-	{
-		// converting in range 0-25
-		int x = (str[i] + key[i]) %26;
-
-		// convert into alphabets(ASCII)
-		x += 'A';
-
-		cipher_text.push_back(x);
-	}
-	return cipher_text;
-}
-
-//Decryption function
-string originalText(string c_text, string key)
-{
-	string orig_text;
-
-	for (int i = 0 ; i < c_text.size(); i++)
-	{
-		int x = (c_text[i] - key[i] + 26) %26;  //Numeric form Conversion
-		x =x+ 'A';                               //ASCII Conversion
-		orig_text.push_back(x);
-	}
-	return orig_text;
-}
-
-//Main function
-int main()
-{
-	string str = "MYNAMEISAMIT",keyword="LOHAN";
-	string key = generateKey(str, keyword);
-	string c_text = cText(str, key);
-
-	cout << "Ciphertext : "	<< c_text << "\n";
-
-	cout << "Decrypted Text : " << originalText(c_text, key);
-	return 0;
+int main(){
+    int i,j,k,n;
+    vector<vector<char> > a(26,vector<char>(26));
+    k=0;
+    n=26;
+    for(i=0;i<n;i++){
+        k=i;
+        for(j=0;j<n;j++){
+            a[i][j]='A'+k;
+            k++;
+            if(k==26)
+                k=0;
+        }
+    }
+    cout<<"Enter the message\n";
+    string s;
+    cin>>s;
+    cout<<"Enter the key\n";
+    string key;
+    cin>>key;
+    k=0;
+    int mod = key.size();
+    for(i=key.size();i<s.size();i++){
+        key+=key[k%mod];
+        k++;
+    }
+    string encrypt;
+    for(i=0;i<s.size();i++){
+        encrypt+= a[s[i]-'A'][key[i]-'A'];
+    }
+    cout<<"Encrypted message: "<<encrypt<<'\n';
+    return 0;
 }
