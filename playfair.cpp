fb09c55e228ec973230a15559b6101aeface0d57
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2021-02-16 16:48:48 +0530

    modified

--- playfair.cpp
+++ playfair.cpp
@@ -1,118 +1,97 @@
-/*
-* Playfair cipher
-* The Playfair cipher was the first practical digraph substitution cipher. The scheme 
-* was invented in 1854 by Charles Wheatstone but was named after Lord Playfair who 
-* promoted the use of the cipher. 
-*
-* Code by Mohammad Faisal
-*/
-#include <iostream>
-#include <string>
- 
+// playfair algorithms
+// mohammad faisal
+// https://ideone.com/UwJyqS
+
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
 using namespace std;
- 
-class playfair
-{
-public:
-    void doIt( string k, string t, bool ij, bool e )
-    {
-	createGrid( k, ij ); getTextReady( t, ij, e );
-	if( e ) doIt( 1 ); else doIt( -1 );
-	display();
+int main(){
+    int i,j,k,n;
+    cout<<"Enter the message"<<endl;
+    string s,origin;
+    getline(cin,origin);
+    cout<<"Enter the key"<<endl;
+    string key;
+    cin>>key;
+    for(i=0;i<origin.size();i++){
+        if(origin[i]!=' ')
+            s+= origin[i];
     }
- 
-private:
-    void doIt( int dir )
-    {
-	int a, b, c, d; string ntxt;
-	for( string::const_iterator ti = _txt.begin(); ti != _txt.end(); ti++ )
-	{
-	    if( getCharPos( *ti++, a, b ) )
-		if( getCharPos( *ti, c, d ) )
-		{
-		    if( a == c )     { ntxt += getChar( a, b + dir ); ntxt += getChar( c, d + dir ); }
-		    else if( b == d ){ ntxt += getChar( a + dir, b ); ntxt += getChar( c + dir, d ); }
-		    else             { ntxt += getChar( c, b ); ntxt += getChar( a, d ); }
-		}
-	}
-	_txt = ntxt;
+    vector<vector<char> > a(5,vector<char>(5,' '));
+    n=5;
+    map<char,int> mp;
+    k=0;
+    int pi,pj;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            while(mp[key[k]]>0&&k<key.size()){
+                k++;
+            }
+            if(k<key.size()){
+                a[i][j]=key[k];
+                mp[key[k]]++;
+                pi=i;
+                pj=j;
+            }
+            if(k==key.size())
+            break;
+        }
+        if(k==key.size())
+            break;
     }
- 
-    void display()
-    {
-	cout << "\n\n OUTPUT:\n=========" << endl;
-	string::iterator si = _txt.begin(); int cnt = 0;
-	while( si != _txt.end() )
-	{
-	    cout << *si; si++; cout << *si << " "; si++;
-	    if( ++cnt >= 26 ) cout << endl, cnt = 0;
-	}
-	cout << endl << endl;
+    k=0;
+    for(;i<n;i++){
+        for(;j<n;j++){
+            while(mp[char(k+'a')]>0&&k<26){
+                k++;
+            }
+            if(char(k+'a')=='j'){
+                j--;
+                k++;
+                continue;
+            }
+            if(k<26){
+                a[i][j]=char(k+'a');
+                mp[char(k+'a')]++;
+            }
+        }
+        j=0;
     }
- 
-    char getChar( int a, int b )
-    {
-	return _m[ (b + 5) % 5 ][ (a + 5) % 5 ];
+    string ans;
+    if(s.size()%2==1)
+        s+="x";
+    for(i=0;i<s.size()-1;i++){
+        if(s[i]==s[i+1])
+            s[i+1]='x';
     }
- 
-    bool getCharPos( char l, int &a, int &b )
-    {
-	for( int y = 0; y < 5; y++ )
-	    for( int x = 0; x < 5; x++ )
-		if( _m[y][x] == l )
-		{ a = x; b = y; return true; }
- 
-	return false;
+    map<char,pair<int,int> > mp2;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            mp2[a[i][j]] = make_pair(i,j);
+        }
     }
- 
-    void getTextReady( string t, bool ij, bool e )
-    {
-	for( string::iterator si = t.begin(); si != t.end(); si++ )
-	{
-	    *si = toupper( *si ); if( *si < 65 || *si > 90 ) continue;
-	    if( *si == 'J' && ij ) *si = 'I';
-	    else if( *si == 'Q' && !ij ) continue;
-	    _txt += *si;
-	}
-	if( e )
-	{
-	    string ntxt = ""; size_t len = _txt.length();
-	    for( size_t x = 0; x < len; x += 2 )
-	    {
-		ntxt += _txt[x];
-		if( x + 1 < len )
-		{
-		    if( _txt[x] == _txt[x + 1] ) ntxt += 'X';
-		    ntxt += _txt[x + 1];
-		}
-	    }
-	    _txt = ntxt;
-	}
-	if( _txt.length() & 1 ) _txt += 'X';
+    
+    for(i=0;i<s.size()-1;i+=2){
+        int y1 = mp2[s[i]].first;
+        int x1 = mp2[s[i]].second;
+        int y2 = mp2[s[i+1]].first;
+        int x2 = mp2[s[i+1]].second;
+        if(y1==y2){
+            ans+=a[y1][(x1+1)%5];
+            ans+=a[y1][(x2+1)%5];
+        }
+        else if(x1==x2){
+            ans+=a[(y1+1)%5][x1];
+            ans+=a[(y2+1)%5][x2];    
+        }
+        else {
+            ans+=a[y1][x2];
+            ans+=a[y2][x1];
+        }
     }
- 
-    void createGrid( string k, bool ij )
-    {
-	if( k.length() < 1 ) k = "KEYWORD"; 
-	k += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; string nk = "";
-	for( string::iterator si = k.begin(); si != k.end(); si++ )
-	{
-	    *si = toupper( *si ); if( *si < 65 || *si > 90 ) continue;
-	    if( ( *si == 'J' && ij ) || ( *si == 'Q' && !ij ) )continue;
-	    if( nk.find( *si ) == -1 ) nk += *si;
-	}
-	copy( nk.begin(), nk.end(), &_m[0][0] );
-    }
- 
-    string _txt; char _m[5][5];
-};
- 
-int main( int argc, char* argv[] )
-{
-    string key, i, txt; bool ij, e;
-    cout << "(E)ncode or (D)ecode? "; getline( cin, i ); e = ( i[0] == 'e' || i[0] == 'E' );
-    cout << "Enter a en/decryption key: "; getline( cin, key ); 
-    cout << "I <-> J (Y/N): "; getline( cin, i ); ij = ( i[0] == 'y' || i[0] == 'Y' );
-    cout << "Enter the text: "; getline( cin, txt ); 
-    playfair pf; pf.doIt( key, txt, ij, e ); return system( "pause" );
+    cout<<ans<<'\n';
+    return 0;
 }
